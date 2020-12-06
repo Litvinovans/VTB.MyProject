9ba093774d2578c1e707a4ccb0c94fb4277d90f3
Natalie <baturinan@gmail.com>
2020-12-06 16:02:01 +0300

    my first commit

---
+++ JAVA/Lesson04/Lesson04.Task1/src/Lesson04/Task1/Main.java
@@ -0,0 +1,11 @@
+package Lesson04.Task1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Написать программу для поиска минимального из двух чисел.
+        int i1=7;
+        int i2=6;
+        if (i1<i2) System.out.println(i1); else System.out.println(i2);
+    }
+}
