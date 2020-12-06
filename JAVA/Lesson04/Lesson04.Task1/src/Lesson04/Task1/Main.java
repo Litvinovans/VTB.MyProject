766f19eb977a1062871f2895f0c34b3f83cef426
Natalie <baturinan@gmail.com>
2020-12-06 16:12:35 +0300

    my first commit

--- JAVA/Lesson04/Lesson04.Task1/src/Lesson04/Task1/Main.java
+++ JAVA/Lesson04/Lesson04.Task1/src/Lesson04/Task1/Main.java
@@ -4,8 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 	// Написать программу для поиска минимального из двух чисел.
+        System.out.println("Lesson04.Task1");
         int i1=7;
         int i2=6;
-        if (i1<i2) System.out.println(i1); else System.out.println(i2);
+        if (i1<i2) {System.out.println("Min number is i1="); System.out.println(i1);}
+        else {System.out.println("Min number is i2=");System.out.println(i2);}
+    // Написать программу, которая описывает введенное число.
+    // Отрицательное оно число или положительное (или нулевое), чётное или нечётное.
+        int j=6;
+
+
+    // Написать программу для вывода на экран таблицы умножения.
+
+    // Написать программу, которая выводит арифметическую или геометрическую прогрессию для N чисел.
+
     }
 }
