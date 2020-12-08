f1cf3072de9f3927c06ae9e7a5a023caa52fe0dc
Natalie <baturinan@gmail.com>
2020-12-08 09:08:11 +0300

    ДЗ 4_2 Реверс массива

--- JAVA/Lesson04/Lesson04.Task1/src/Lesson04/Task1/Main.java
+++ JAVA/Lesson04/Lesson04.Task1/src/Lesson04/Task1/Main.java
@@ -3,35 +3,38 @@ package Lesson04.Task1;
 public class Main {
 
     public static void main(String[] args) {
-        toLeft();
-    }
-
-    public static void toLeft() {
+        //toLeft();
+        int[] array = new int[9];
         // заполняем массив array числами
         System.out.println("Начальный массив");
-        int[][] array = new int[10][10];
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                array[i][j] = ((int) (Math.random() * 100));
-                System.out.print(array[i][j]+" ");
-            }
-            System.out.println();
+            array[i] = ((int) (Math.random() * 100));
+            System.out.print(array[i]+" ");
         }
+        System.out.println();
 
-        // сдвигаем элементы массива влево, последний элемент каждой строки заполняем 0
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length-1; j++) {
-                array[i][j] = array[i][j+1];
-            }
-            array[i][array.length-1] = 0;
-        }
+        MyReverseArray(array);
+
         // печатаем новый массив
         System.out.println("Преобразованный массив");
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                System.out.print(array[i][j]+" ");
+        for (int i = 0; i < array.length; i++) System.out.print(array[i]+" ");
+    }
+
+    private static void MyReverseArray(int[] array) {
+        int temp;
+        if (array.length % 2 == 0) {
+            for (int i = 0; i < (array.length / 2); i++) {
+                temp = array[i];
+                array[i] = array[array.length - i - 1];
+                array[array.length - i - 1] = temp;
             }
-            System.out.println();
+        }
+        else {
+            for (int i = 0; i < (array.length / 2-1); i++) {
+                temp = array[i];
+                array[i] = array[array.length - i - 1];
+                array[array.length - i - 1] = temp;
+            }
         }
     }
 }
