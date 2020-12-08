161c4998dd19a26f8ee68b376418705dc2c12dcd
Natalie <baturinan@gmail.com>
2020-12-08 19:57:18 +0300

    ДЗ 4_2 Реверс массива

--- JAVA/Lesson04/Lesson04.Task1/src/Lesson04/Task1/Main.java
+++ JAVA/Lesson04/Lesson04.Task1/src/Lesson04/Task1/Main.java
@@ -3,38 +3,5 @@ package Lesson04.Task1;
 public class Main {
 
     public static void main(String[] args) {
-        //toLeft();
-        int[] array = new int[9];
-        // заполняем массив array числами
-        System.out.println("Начальный массив");
-        for (int i = 0; i < array.length; i++) {
-            array[i] = ((int) (Math.random() * 100));
-            System.out.print(array[i]+" ");
-        }
-        System.out.println();
-
-        MyReverseArray(array);
-
-        // печатаем новый массив
-        System.out.println("Преобразованный массив");
-        for (int i = 0; i < array.length; i++) System.out.print(array[i]+" ");
-    }
-
-    private static void MyReverseArray(int[] array) {
-        int temp;
-        if (array.length % 2 == 0) {
-            for (int i = 0; i < (array.length / 2); i++) {
-                temp = array[i];
-                array[i] = array[array.length - i - 1];
-                array[array.length - i - 1] = temp;
-            }
-        }
-        else {
-            for (int i = 0; i < (array.length / 2-1); i++) {
-                temp = array[i];
-                array[i] = array[array.length - i - 1];
-                array[array.length - i - 1] = temp;
-            }
-        }
     }
 }
