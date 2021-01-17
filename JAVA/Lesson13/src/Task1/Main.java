3f7d813d3721e81682ffb4c09710477606b27b66
Natalie <baturinan@gmail.com>
2021-01-17 16:47:01 +0300

    ДЗ 11. Кассовый чек_версия 2

--- JAVA/Lesson13/src/Task1/Main.java
+++ JAVA/Lesson13/src/Task1/Main.java
@@ -1,36 +1,44 @@
 package Task1;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 //ДЗ 11. Кассовый чек
 public class Main {
     public static void main(String[] args) {
-        String ss = "";
+        String s;
+        int i = 1;
+        String name="";
         double amount = 0;
         double price = 0;
         double finalprice = 0;
         double wholeprice = 0;
         double scale = Math.pow(10, 2);
-        System.out.print("Наименование         Цена   Кол-во    Стоимость\n");
-        System.out.print("===============================================\n");
-        try {
-            Scanner sc = new Scanner(new File("/Users/natalia/Desktop/JAVA/Lesson13/products.txt"));
-            while (sc.hasNext()) {
-                ss = sc.nextLine();
-                if (sc.hasNextDouble()) {
-                    amount = sc.nextDouble();
+        try(BufferedReader br = new BufferedReader(new FileReader("/Users/natalia/Desktop/JAVA/Lesson13/products.txt")))
+        {
+            while((s=br.readLine())!=null){
+                switch (i) {
+                    case (1):
+                        name=s;
+                        i++;
+                        break;
+                    case (2):
+                        amount=Double.parseDouble(s);
+                        i++;
+                        break;
+                    case (3):
+                        price=Double.parseDouble(s);
+                        finalprice = Math.ceil((amount * price)*scale) / scale;
+                        wholeprice = wholeprice + finalprice;
+                        System.out.printf("%-15s %9.2f x %6.3f =%11.2f\n",name,price,amount,finalprice);
+                        i=1;
+                        break;
                 }
-                if (sc.hasNextDouble()) {
-                    price = sc.nextDouble();
-                }
-                finalprice = Math.ceil((amount * price)*scale) / scale;
-                wholeprice = wholeprice + finalprice;
-                System.out.printf("%-15s %9.2f x %6.3f =%11.2f\n",ss,price,amount,finalprice);
-                ss = sc.nextLine();
             }
-        } catch (IOException e) {
+        }
+        catch(IOException e){
+
             System.out.println(e.getMessage());
         }
         System.out.print("===============================================\n");
